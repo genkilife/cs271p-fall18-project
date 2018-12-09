df7bb900bb9a6e1c6d66a8bf96d0569a5821e0bf
genkilife <genkilifegogo@gmail.com>
2018-12-08 21:06:23 -0800

    revise format

--- Minesweeper_Python/src/MyAI.py
+++ Minesweeper_Python/src/MyAI.py
@@ -22,7 +22,7 @@ class Term():
     def __lt__(self, other):
         return self.coefficient < other.coefficient
     def __str__(self):
-        return str(self.coefficient) + " " + str(self.var) 
+        return "( " + str(self.coefficient) + " " + str(self.var) + " )"
 
 class PolynomialEqation():
     def __init__(self, numOfBomb=None, terms=None):
@@ -65,9 +65,10 @@ class PolynomialEqation():
         return ret
     
     def __str__(self):
-        ret = ""
-        for term in self.terms:
-            ret = ret + str(term)
+        assert(len(self.terms) > 0)
+        ret = str(self.terms[0])
+        for term in self.terms[1:]:
+            ret = ret + " , " + str(term)
         ret = ret + " = " + str(self.numOfBomb)
         return ret
 
@@ -293,7 +294,7 @@ class MyAI(AI):
                     polynomialEquation.numOfBomb = polynomialEquation.numOfBomb - 1
                 elif self.tileInfo[self.rowTotal-1-ny][nx] == -1:
                     # if covered, add one term
-                    term = Term((nx, ny), 1)
+                    term = Term(1, (nx, ny))
                     polynomialEquation.terms.append(term)
             else:
                 continue
